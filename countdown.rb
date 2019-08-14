424837d75be17f575e513fc3ffbf37de3fcc95a9
Maverick Adolfo <merkonical@gmail.com>
2019-08-14 19:53:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown
-  n = 10 
-    while n > 0     
-      puts "#{n} SECOND(S)!"
-      n -= 1 
-    end
+   
+  while n > 0     
+    puts "#{n} SECOND(S)!"
+    n -= 1 
+  end
   return "HAPPY NEW YEAR!" 
 end
